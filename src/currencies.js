6f1a434ef87f7762f4ea8cfef7d2c7b5d2e5fa99
Dariusz Podczasik <boostertech@mail.com>
2023-12-27 20:57:41 +0000

    currencies.js file restored

---
+++ src/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+  {
+    abbreviation: "EUR",
+    name: "Euro",
+    rate: 4.47,
+  },
+  {
+    abbreviation: "USD",
+    name: "American dollar",
+    rate: 4.1,
+  },
+  {
+    abbreviation: "GBP",
+    name: "British Pound",
+    rate: 5.22,
+  },
+];
